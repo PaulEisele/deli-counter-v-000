94fd6802af6339dc98edbaaff904b839b593e9b2
paulheisele@gmail.com <paulheisele@gmail.com>
2018-06-20 18:49:21 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,18 +2,21 @@
 katz_deli = []
 
 def line(katz_deli)
-if katz_deli.length ==0
+if katz_deli.length ==[]
   puts "The line is currently empty."
 else
   puts "#{name}, #{katz_deli+1}"
 end
 
 def take_a_number(katz_deli, name)
-  puts"#{name}, #{position}"
+  katz_deli.push(name)
+  puts"#{name}, You are number #{katz_deli.length+1} in line"
 end
 
 def now_serving(katz_deli)
-  if katz_deli[]==-1
+  if katz_deli[]==[]
     puts "There is nobody waiting to be served!"
   else
-    puts "#{name}"
+    puts "Now serving #{katz_deli.shift}"
+  end
+end
