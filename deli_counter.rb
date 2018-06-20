c7e329e529035d8ff750bdfd5210690f516238c4
paulheisele@gmail.com <paulheisele@gmail.com>
2018-06-20 18:57:43 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
   if katz_deli == []
     puts "The line is currently empty."
   else
-    katz_deli.collect!.with_index{|name, i| "#{i+1}, #{name}"}
+    katz_deli.collect!.with_index{|name. i| "#{i+1}, #{name}"}
     puts "Line is currently: #{katz_deli.join.chop}"
   end
 end
